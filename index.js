faa95f1a2bbdc11438dd831e1902041083915b12
Tomiwa Folorunso <78617644+tommichaels@users.noreply.github.com>
2023-09-28 19:05:05 +0100

    completed project setup

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const canvas = document.querySelector('canvas');
 
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+ctx.fillRect(0, 0, canvas.width, canvas.height);
